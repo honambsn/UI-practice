121547af0748941c83c72166b53c4f2375abc4d7
honam <75104291+honambsn@users.noreply.github.com>
2024-09-18 07:46:40 +0700

    add: added

---
+++ Menu Hover Effects/script.js
@@ -0,0 +1,11 @@
+let text = document.querySelectorAll('ul li a').forEach(
+    (text) => {
+        text.innerHTML = text.innerText.split('').map((letters,i) => `<span style="transition-delay:${i*50}ms">${letters}</span>`).join('');
+    }
+);
+
+let cursor = document.querySelector('#cursor');
+document.addEventListener('mousemove', (e) =>{
+    cursor.style.top = e.pageY + 'px';
+    cursor.style.left = e.pageX + 'px';
+})
