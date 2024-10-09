c5c9bfc2152013a3f56224d9caa87e51bdce4189
honam <75104291+honambsn@users.noreply.github.com>
2024-10-09 08:00:16 +0700

    add: added

---
+++ Drag n Drop/script.js
@@ -0,0 +1,12 @@
+let boxes = document.querySelectorAll('.box');
+let glow = document.querySelector('.glow');
+
+boxes.forEach((box) =>{
+    box.addEventListener("dragover", (e) =>{
+        e.preventDefault();
+    })
+
+    box.addEventListener("drop", () =>{
+        box.appendChild(glow);
+    })
+})
