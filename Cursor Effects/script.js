2bb0704e4d7cd8f1bac8efd15db922b8168072c4
honam <75104291+honambsn@users.noreply.github.com>
2024-11-06 06:57:02 +0700

    add: added

---
+++ Cursor Effects/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("mousemove", function(e){
+    let body = document.querySelector("body");
+    let bubbles = document.createElement("span");
+    bubbles.style.left = -75 + e.offsetX + "px";
+    bubbles.style.top = -75 + e.offsetY + "px";
+    body.appendChild(bubbles);
+    setTimeout(function(){
+        bubbles.remove();
+    }, 2000);
+})
