7d04a1c8ad348cd1925665bf0a7426d66a848829
honam <75104291+honambsn@users.noreply.github.com>
2025-02-25 15:10:54 +0700

    update: add copy function, add more options later

--- Gen Pass/script.js
+++ Gen Pass/script.js
@@ -1,3 +1,5 @@
+var alertBox = document.querySelector('.alertBox');
+
 function getPassword() {
     var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?/~`";
     var passwordLength = 16;
@@ -8,5 +10,42 @@ function getPassword() {
         password += chars.substring(randomNumber, randomNumber + 1);
     }
     document.getElementById("password").value = password;
+    //alertBox.innerHTML = "New password copied to clipboard!" + "<br>" + password;
 }
 
+
+function copyPassword() {
+    var copiedText = document.getElementById("password");
+
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(copiedText.value)
+          .then(function() {
+            alertBox.innerHTML = "New password copied to clipboard!";
+          })
+          .catch(function(err) {
+            copiedText.select();
+            
+            try {
+                document.execCommand('copy');
+                alertBox.innerHTML = "New password copied to clipboard";
+            }
+            catch (err) {
+                alertBox.innerHTML = "Oops, unable to copy!";
+            }
+          });
+        } else {
+            alertBox.innerHTML = "Clipboard API not supported!";
+      }
+    
+    alertBox.classList.add('active');
+    
+    setTimeout(function() {
+        alertBox.classList.remove('active');
+    }, 1000);
+    
+    document.addEventListener('keydown', function(event) {
+        if (event.key == 'Escape') {
+            alertBox.classList.remove('active');
+        }
+    });
+}
