b748c8b2165cbcb6568b6c779c557bc42c4675e5
honam <75104291+honambsn@users.noreply.github.com>
2025-07-19 07:28:21 +0700

    update display details err, promise err

--- Pokemon API Practice/test.js
+++ Pokemon API Practice/test.js
@@ -389,12 +389,21 @@ document.addEventListener('keydown', (event) => {
               createBlur(); // Create a blur effect while fetching
               console.log(`Fetching card with ID: ${randomID}`);
               fetchCard(randomID).then(() => {
-                console.log(`Card with ID ${randomID} fetched successfully.`);
+                const card_data = data.data.find(item => item.id === randomID);
+
+                console.log(`Card with ID ${randomID} fetched successfully.`);                
+
+                console.log(`Card ---data:`, card_data); // Log the fetched card data
               })
               .catch(error => {
                 console.error(`Error fetching card with ID ${randomID}:`, error);
               })
               .finally(() => {
+                const details = document.getElementById('details');
+                details.style.display = 'block'; // Show the details section
+
+                //typeData(data, outputElement)
+                typeData(card_data, details);
                 console.log(`Finished fetching card with ID: ${randomID}`);
                 isFetching = false; // Reset the flag after fetching
 
@@ -548,7 +557,7 @@ function getPokemonRandom(arr){
 }
 
 // Example usage
-console.log(getPokemonRandom(arr)); // This will log a random Pokémon name from the array
+//console.log(getPokemonRandom(arr)); // This will log a random Pokémon name from the array
 
 // Card ID: hgss4-1,xy5-1,pl1-1,dp3-1,det1-1,dv1-1,mcd19-1,pl3-1,ex12-1,ex3-1,ru1-1,ecard2-H1,base4-1,hgss1-1,ex11-1,ex7-1,gym1-1,base3-1,sm9-1,xy2-1,pop6-1,ecard2-H2,hgss4-2,pl1-2,ex3-2,dp3-2,base3-2,pl3-2,ex11-2,pop6-2,xy2-2,base4-2,gym1-2,hgss1-2,det1-2,sm9-2,ex12-2,ex7-2,ru1-2,mcd19-2,xy5-2,xy11-1,ex14-1,sm12-1,bw1-1,sm115-1,si1-1,dp1-1,sm3-1,pop7-1,gym2-1,col1-1,ex9-1,bw10-1,swsh35-1,xy7-1,sm1-1,ex13-1,dv1-2,ecard2-H3,sm12-2,ex9-2,bw1-2,base4-3,ru1-3,xy5-3,pop6-3,si1-2,ex11-3,pl3-3,base3-3,hgss1-3,ex13-2,hgss4-3,xy7-2,bw10-2,ex7-3,ex12-3,swsh35-2,gym2-2,xy2-3,xy11-2,det1-3,ex3-3,dv1-3,sm9-3,pop7-2,sm1-2,mcd19-3,col1-2,pl1-3,sm115-2,gym1-3,dp3-3,sm2-1,base5-1,xy4-1,bw11-1,ex4-1,bw4-1,bw9-1,base1-1,pop3-1,dp4-1,bw7-1,ex8-1,bw5-1,pop5-1,sm5-1,xy10-1,sm8-1,hgss2-1,dpp-DP01,swsh1-1,basep-1,swsh4-1,xy1-1,pop1-1,bw8-1,base6-1,pop9-1,pop4-1,neo4-1,neo3-1,pl4-1,mcd19-4,sm115-3,base3-4,ex9-3,pop7-3,pop6-4,det1-4,ex4-2,hgss1-4,xy1-2,ex8-2,bw5-2,dpp-DP02,sm2-2,pop4-2,bw7-2,xy4-2,col1-3,ex12-4,bw11-2,pl1-4,swsh35-3,base5-2,ecard2-H4,neo4-2,ex3-4,xy2-4,pop5-2,base6-2,dv1-4,ex11-4,dp4-2,dp1-2,gym2-3,pop1-2,si1-3,dp3-4,base4-4,pl4-2,bw8-2,pop3-2,xy11-3,sm3-2,swsh1-2,pop9-2,xy7-3,bw10-3,bw4-2,sm9-4,hgss4-4,ex13-3,sm5-2,sm75-1,ex10-1,bw6-1,si1-4,bw5-3,dp1-3,xy11-4,hgss4-5,sm12-3,ex7-4,sm5-3,swsh1-3,pl3-4,hgss1-5,ex12-5,pl4-3,bw10-4,pop4-3,ex3-5,swsh4-2,xy5-4,ex9-4,ex11-5,ex4-3,base3-5,dv1-5,dp4-3,dp3-5,bw1-3,ecard2-H5,pop1-3,gym2-4,xy4-3,ex10-2,xy7-4,pl1-5,pop3-3,ex8-3,basep-2,base6-3,bw4-3,hgss2-2,bw11-3,pop7-4,ru1-4,neo3-2,mcd19-5,sm8-2,sm75-2,sm9-5,bw6-2,neo4-3,bw7-3,swsh35-4,pop9-3,dpp-DP03,xy10-2,sm115-4,base1-2,sm3-3,xy2-5,xy1-3,ex14-2,det1-5,pl1-6,dpp-DP04,xy5-5,pop6-5,xy2-6,ex14-3,dv1-6,ex8-4,dp4-4
 
@@ -706,7 +715,16 @@ function setupConsoleNew() {
 // });
 
 
+function typeData(data, outputElement) {
+  const target = document.getElementById(outputElement);
+  let i = 0;
 
+  if (i < data.length){
+    target.textContent += data.charAt(i);
+    i++;
+    setTimeout(typeData(data, outputElement), 200); // Adjust the speed of typing here
+  }
+}
 
 
 
